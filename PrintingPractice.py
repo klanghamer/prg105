4f209d4f6ea0284b82bf2a275ac134331e010943
Kate Langhamer <42592073+klanghamer@users.noreply.github.com>
2018-09-10 13:58:02 -0500

    Added Printing Practice

---
+++ PrintingPractice.py
@@ -0,0 +1,20 @@
+# Printing the poem Where the Sidewalk Ends
+
+print("This is a place where the sidewalk ends")
+print("And before the street begins,")
+print("And there the grass grows soft and white,")
+print("And there the sun burns crimson bright,")
+print("And there the moon-bird rests from his flight")
+print("To cool in the peppermint wind.\n")
+
+print("Let us leave this place where the smoke blows black")
+print("And the dark street winds and bends.")
+print("Past the pits where the asphalt flowers grow")
+print("We shall walk with a walk that is measured and slow,")
+print("And watch where the chalk-white arrows go")
+print("To the place where the sidewalk ends.\n")
+
+print("Yes we'll walk with a walk that is measured and slow,")
+print("And we'll go where the chalk-white arrows go,")
+print("For the children, they mark, and the children, they know")
+print("The place where the sidewalk ends.")
