ebe49a7f037a2054ed0969e57954cbf140f0e703
Kate Langhamer <42592073+klanghamer@users.noreply.github.com>
2018-09-20 10:47:39 -0500

    Updated 5.8

--- ChapterFivePractice.py
+++ ChapterFivePractice.py
@@ -151,26 +151,25 @@ main5()
 # TODO 5.8 value returning functions
 # Complete the following program, remove the """  """ before testing
 
-# need help
-
 def main6():
     print("This program will calculate your BMI")
     height = float(input("What is your height in inches?  "))
     weight = float(input("What is your weight in pounds"))
+    answer = bmi(height, weight)
+    print(format(answer, '.1f'))
 
     # TODO call the bmi function and assign the result to a variable named answer
-
     # TODO print the variable answer, make sure to format it to 1 decimal place
-
     # TODO modify the bmi function to accept the height and weight
     # read the code to determine the parameter names
 
 
-def bmi(height, weight):
+def bmi(height_inches, weight_pounds):
     # BMI = (Weight in Pounds / (Height in inches x Height in inches)) x 703
-    patient_bmi = (weight / (height * height)) * 703
+    print(height_inches, weight_pounds)
+    patient_bmi = (weight_pounds / (height_inches * height_inches)) * 703
 
-
+    return patient_bmi
     # TODO send the patient_bmi value back to main6
 
 
