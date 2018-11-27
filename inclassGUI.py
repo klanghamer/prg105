f31f27762473279843ffec0c007e7e30dc50db29
Kate Langhamer <42592073+klanghamer@users.noreply.github.com>
2018-11-27 10:23:03 -0600

    Update inclassGUI.py

--- inclassGUI.py
+++ inclassGUI.py
@@ -1,31 +1,34 @@
+"""
+    Create a CRUD application with a GUI interface
+"""
+
 import pickle
 import tkinter
 import tkinter.messagebox
 
 
 class CrudGUI:
-    def __init__(self, master, cust):
+    def __init__(self, master, input_file):
         self.master = master
         self.master.title('Welcome Menu')
-        self.customer_list = cust
-        self.radio_var = 0
+        self.input_file = input_file
 
         self.top_frame = tkinter.Frame(self.master)
         self.bottom_frame = tkinter.Frame(self.master)
 
         self.radio_var = tkinter.IntVar()
         self.radio_var.set(1)
-        self.radio_var.set(2)
-        self.radio_var.set(3)
-        self.radio_var.set(4)
 
         # create radio buttons
-        self.look = tkinter.Radiobutton(self.top_frame, text='Look Up Customer', variable=self.radio_var, value=1)
-        self.add = tkinter.Radiobutton(self.top_frame, text='Add Customer', variable=self.radio_var, value=2)
-        self.change = tkinter.Radiobutton(self.top_frame, text='Change Customer Info', variable=self.radio_var, value=3)
-        self.delete = tkinter.Radiobutton(self.top_frame, text='Delete a Customer', variable=self.radio_var, value=4)
+
+        self.look = tkinter.Radiobutton(self.top_frame, text="Look up customer", variable=self.radio_var, value=1)
+        self.add = tkinter.Radiobutton(self.top_frame, text="Add a customer", variable=self.radio_var, value=2)
+        self.change = tkinter.Radiobutton(self.top_frame, text="Change customer information", variable=self.radio_var,
+                                          value=3)
+        self.delete = tkinter.Radiobutton(self.top_frame, text="Delete a customer", variable=self.radio_var, value=4)
 
         # pack radio buttons
+
         self.look.pack(anchor='w', padx=20)
         self.add.pack(anchor='w', padx=20)
         self.change.pack(anchor='w', padx=20)
@@ -33,77 +36,87 @@ class CrudGUI:
 
         # create ok and quit buttons
 
-        self.ok_button = tkinter.Button(self.bottom_frame, text='OK', command=self.open_menu)
-        self.quit_button = tkinter.Button(self.bottom_frame, text='Quit', command=self.master.destroy)
+        self.ok_button = tkinter.Button(self.bottom_frame, text="OK", command=self.open_menu)
+        self.quit_button = tkinter.Button(self.bottom_frame, text="QUIT", command=self.master.destroy)
 
         # pack buttons
         self.ok_button.pack(side='left')
         self.quit_button.pack(side='left')
 
         # pack the frames
-
         self.top_frame.pack()
         self.bottom_frame.pack()
 
     def open_menu(self):
         if self.radio_var.get() == 1:
-            search = LookGUI(self.master, self.customer_list)
+            LookGUI(self.master, self.input_file)
         elif self.radio_var.get() == 2:
-            search = AddGUI(self.master, self.customer_list)
+            AddGUI(self.master, self.input_file)
         elif self.radio_var.get() == 3:
-            search = UpdateGUI(self.master, self.customer_list)
+            UpdateGUI(self.master, self.input_file)
         else:
-            search = DeleteGUI(self.master, self.customer_list)
+            DeleteGUI(self.master, self.input_file)
 
 
 class LookGUI:
-    def __init__(self, master, look_cust):
+    def __init__(self, master, input_file):
         self.master = master
+        self.input_file = input_file
         self.look = tkinter.Toplevel(master)
-        self.look.title('Search for Customer')
-        self.customer_list = look_cust
+        self.look.title("Search for customer")
         self.top_frame = tkinter.Frame(self.look)
         self.middle_frame = tkinter.Frame(self.look)
         self.bottom_frame = tkinter.Frame(self.look)
 
-        self.search_label = tkinter.Label(self.top_frame, text='Enter customers name to look up:')
+        # widgets for top frame
+        self.search_label = tkinter.Label(self.top_frame, text='Enter customer name to look for: ')
         self.search_entry = tkinter.Entry(self.top_frame, width=15)
 
+        # pack top frame
         self.search_label.pack(side='left')
         self.search_entry.pack(side='left')
 
+        # widget for middle frame
         self.value = tkinter.StringVar()
-        self.info = tkinter.Label(self.middle_frame, text='Results: ')
-        self.result_label = tkinter.Label(self.middle_frame, textvariable=self.value)
+        self.results_label = tkinter.Label(self.middle_frame, text='Results: ')
+        self.results = tkinter.Label(self.middle_frame, textvariable=self.value)
 
-        self.info.pack(side='left')
-        self.result_label.pack(side='left')
+        # pack middle frame
+        self.results_label.pack(side='left')
+        self.results.pack(side='left')
 
-        self.search_button = tkinter.Button(self.bottom_frame, text='Add', command=self.search)
-        self.back_button = tkinter.Button(self.bottom_frame, text='Main Menu', command=self.back)
+        # buttons for bottom frame
+        self.search_button = tkinter.Button(self.bottom_frame, text='Search', command=self.search)
+        self.quit_button = tkinter.Button(self.bottom_frame, text='QUIT', command=self.look.destroy)
 
+        # pack bottom frame
         self.search_button.pack(side='left')
-        self.back_button.pack(side='left')
+        self.quit_button.pack(side='left')
 
+        # pack frames
         self.top_frame.pack()
         self.middle_frame.pack()
         self.bottom_frame.pack()
 
     def search(self):
-        name = self.search_entry.get()
-        result = self.customer_list.get(name, 'Not Found')
-        self.value.set(result)
+        try:
+            my_input = open(self.input_file, 'rb')
+            customers = pickle.load(my_input)
+            my_input.close()
+        except(FileNotFoundError, IOError):
+            customers = {}
 
-    def back(self):
-        self.look.destroy()
+        name = self.search_entry.get()
+        result = customers.get(name, 'That name was not found')
+        self.value.set(result)
 
 
 class AddGUI:
-    def __init__(self, master, add_cust):
+    def __init__(self, master, input_file):
         self.master = master
+        self.input_file = input_file
         self.add = tkinter.Toplevel(master)
         self.add.title('Add a Customer')
-        self.customer_list = add_cust
         self.top_frame = tkinter.Frame(self.add)
         self.middle_frame = tkinter.Frame(self.add)
         self.bottom_frame = tkinter.Frame(self.add)
@@ -125,7 +138,7 @@ class AddGUI:
         self.info.pack(side='left')
         self.result_label.pack(side='left')
 
-        self.add_button = tkinter.Button(self.bottom_frame, text='Add', command=self.add)
+        self.add_button = tkinter.Button(self.bottom_frame, text='Add', command=self.add_value)  # shadow error
         self.back_button = tkinter.Button(self.bottom_frame, text='Main Menu', command=self.back)
 
         self.add_button.pack(side='left')
@@ -135,33 +148,46 @@ class AddGUI:
         self.middle_frame.pack()
         self.bottom_frame.pack()
 
-    def add(self):
+    def add_value(self):  # Don't let shadow error happen
+        tkinter.messagebox.showinfo("Hey Listen", "add")
+        try:
+            customer_file = open(self.input_file, 'rb')
+            customers = pickle.load(customer_file)
+            customer_file.close()
+        except FileNotFoundError:
+            customers = {}
+
         name = self.add_entry.get()
         email = self.add_entry2.get()
-        result = self.customer_list.get(name, email, 'Customer Added')
-        self.value.set(result)
-        # customers_add[add_email] = add_name
-        # save_file(customers_add)
+        customers[email] = name
+
+        file = open("customers.dat", 'wb')
+        pickle.dump(customers, file)
+        file.close()
 
     def back(self):
         self.add.destroy()
 
 
 class UpdateGUI:
-    def __init__(self, master, update_cust):
+    def __init__(self, master, input_file):
         self.master = master
+        self.input_file = input_file
         self.update = tkinter.Toplevel(master)
         self.update.title('Update a Customer')
-        self.customer_list = update_cust
         self.top_frame = tkinter.Frame(self.update)
         self.middle_frame = tkinter.Frame(self.update)
         self.bottom_frame = tkinter.Frame(self.update)
 
-        self.update_label = tkinter.Label(self.top_frame, text='Enter customers name: ')
+        self.update_label = tkinter.Label(self.top_frame, text='Enter customers email: ')
         self.update_entry = tkinter.Entry(self.top_frame, width=15)
+        self.update_label2 = tkinter.Label(self.top_frame, text='Enter new email: ')
+        self.update_entry2 = tkinter.Entry(self.top_frame, width=15)
 
         self.update_label.pack(side='left')
         self.update_entry.pack(side='left')
+        self.update_label2.pack(side='left')
+        self.update_entry2.pack(side='left')
 
         self.value = tkinter.StringVar()
         self.info = tkinter.Label(self.middle_frame, text='Results: ')
@@ -170,37 +196,56 @@ class UpdateGUI:
         self.info.pack(side='left')
         self.result_label.pack(side='left')
 
-        self.add_button = tkinter.Button(self.bottom_frame, text='Update', command=self.update)
+        self.update_button = tkinter.Button(self.bottom_frame, text='Update', command=self.update_value)  # shadow error
         self.back_button = tkinter.Button(self.bottom_frame, text='Main Menu', command=self.back)
 
-        self.add_button.pack(side='left')
+        self.update_button.pack(side='left')
         self.back_button.pack(side='left')
 
         self.top_frame.pack()
         self.middle_frame.pack()
         self.bottom_frame.pack()
 
-    def update(self):
-        name = self.update_entry.get()
-        result = self.customer_list.get(name, 'Customers Information Updated')
-        self.value.set(result)
-        # customer_change[change_name] = name_change
+    def update_value(self):  # Don't let shadow error happen
+        tkinter.messagebox.showinfo("Hey Listen", "update")
+
+        try:
+            customer_file = open(self.input_file, 'rb')
+            customers = pickle.load(customer_file)
+            customer_file.close()
+        except FileNotFoundError:
+            customers = {}
+
+        change_name = self.update_entry2.get()
+        customer_change = self.update_entry.get()
+        customers[customer_change] = change_name
+
+        file = open("customers.dat", 'wb')
+        pickle.dump(customers, file)
+        file.close()
 
     def back(self):
         self.update.destroy()
 
 
+def main():
+    input_file = "customers.dat"
+    root = tkinter.Tk()
+    CrudGUI(root, input_file)
+    root.mainloop()
+
+
 class DeleteGUI:
-    def __init__(self, master, delete_cust):
+    def __init__(self, master, input_file):
         self.master = master
+        self.input_file = input_file
         self.delete = tkinter.Toplevel(master)
-        self.delete.title('Delete a Customer')
-        self.customer_list = delete_cust
+        self.delete.title('Update a Customer')
         self.top_frame = tkinter.Frame(self.delete)
         self.middle_frame = tkinter.Frame(self.delete)
         self.bottom_frame = tkinter.Frame(self.delete)
 
-        self.delete_label = tkinter.Label(self.top_frame, text='Enter customers name: ')
+        self.delete_label = tkinter.Label(self.top_frame, text='Enter customers email you want to delete: ')
         self.delete_entry = tkinter.Entry(self.top_frame, width=15)
 
         self.delete_label.pack(side='left')
@@ -213,7 +258,7 @@ class DeleteGUI:
         self.info.pack(side='left')
         self.result_label.pack(side='left')
 
-        self.delete_button = tkinter.Button(self.bottom_frame, text='Delete', command=self.delete)
+        self.delete_button = tkinter.Button(self.bottom_frame, text='Update', command=self.delete_value)  # shadow error
         self.back_button = tkinter.Button(self.bottom_frame, text='Main Menu', command=self.back)
 
         self.delete_button.pack(side='left')
@@ -223,25 +268,37 @@ class DeleteGUI:
         self.middle_frame.pack()
         self.bottom_frame.pack()
 
-    def delete(self):
-        name = self.delete_entry.get()
-        result = self.customer_list.get(name, 'Customers Information Deleted')
+    def delete_value(self):  # Don't let shadow error happen
+
+        try:
+            customer_file = open(self.input_file, 'rb')
+            customers = pickle.load(customer_file)
+            customer_file.close()
+        except FileNotFoundError:
+            customers = {}
+
+        name_delete = self.delete_entry.get()
+
+        if name_delete in customers:
+            del customers[name_delete]
+
+        result = customers.get(name_delete, 'Customer Deleted')
         self.value.set(result)
-        # customer_delete[name_delete]
+
+        file = open("customers.dat", 'wb')
+        pickle.dump(customers, file)
+        file.close()
 
     def back(self):
         self.delete.destroy()
 
-def main():
-    try:
-        input_file = open("customers.dat", 'rb')
-        customers = pickle.load(input_file)
-    except (FileNotFoundError, IOError):
-        customers = {}
 
-    root = tkinter.Tk()
-    menuGui = CrudGUI(root, customers)
-    root.mainloop()
-
+"""
+def save_file(customer_pickle):
+    # pickle and dump the file
+    input_file = open('customers_file.dat', 'wb')
+    pickle.dump(customer_pickle, input_file)
+    input_file.close()
+"""
 
 main()
