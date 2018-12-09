8d6a1938a3a828fcde03024d5b47a106aed33826
Kate Langhamer <42592073+klanghamer@users.noreply.github.com>
2018-12-09 15:26:55 -0600

    Race Main Menu

--- testrace.py
+++ testrace.py
@@ -1,123 +1,80 @@
 import tkinter
 import tkinter.messagebox
+import human
 
-"""
-class Human:
-    def __init__(self, master, input_file):
+
+class MainMenu:
+    def __init__(self, master):
         self.master = master
-        self.input_file = input_file
-        self.human = tkinter.Toplevel(master)
-        self.human.title('Race - Human')
-        self.top_frame = tkinter.Frame(self.human)
-        self.middle_frame = tkinter.Frame(self.human)
-        self.bottom_frame = tkinter.Frame(self.human)
+        self.master.title('Welcome Menu')
 
-        self.human_label = tkinter.Label(self.top_frame, text='Enter customers name: ')
-        self.human_entry = tkinter.Entry(self.top_frame, width=15)
-        self.human_label2 = tkinter.Label(self.top_frame, text='Enter customers email: ')
-        self.human_entry2 = tkinter.Entry(self.top_frame, width=15)
+        self.upper_frame = tkinter.Frame(self.master)
+        self.top_frame = tkinter.Frame(self.master)
+        self.bottom_frame = tkinter.Frame(self.master)
 
-        self.human_label.pack(side='left')
-        self.human_entry.pack(side='left')
-        self.human_label2.pack(side='left')
-        self.human_entry2.pack(side='left')
+        # Top Image ----------------------------------------------------
+        self.canvas = tkinter.Canvas(self.master, width=400, height=125)
+        self.pop = tkinter.Canvas(self.master)
+        topimage = tkinter.PhotoImage(file='neverwinter.png')
 
-        self.value = tkinter.StringVar()
-        self.info = tkinter.Label(self.middle_frame, text='Results: ')
-        self.result_label = tkinter.Label(self.middle_frame, textvariable=self.value)
+        self.canvas.create_image(200, 75, image=topimage)
+        self.canvas.pack()
 
-        self.info.pack(side='left')
-        self.result_label.pack(side='left')
+        # Title / Directions ----------------------------------------------------
 
-        self.human_button = tkinter.Button(self.bottom_frame, text='Add', command=self.human_value)  # shadow error
-        self.back_button = tkinter.Button(self.bottom_frame, text='Main Menu', command=self.back)
+        self.title_label = tkinter.Label(self.upper_frame, text='Welcome Adventurer!\n Please choose your race. ')
+        self.title_label.pack(side='left')
 
-        self.human_button.pack(side='left')
-        self.back_button.pack(side='left')
+        # Radio Options ----------------------------------------------------
+        self.radio_var = tkinter.IntVar()
+        self.radio_var.set(1)
 
+        self.human = tkinter.Radiobutton(self.top_frame, text='Human: +2 STR, +2 DEX',
+                                         variable=self.radio_var, value=1)
+        self.half_orc = tkinter.Radiobutton(self.top_frame, text='Half-Orc: +2 DEX, +2 CON',
+                                            variable=self.radio_var, value=2)
+        self.sun_elf = tkinter.Radiobutton(self.top_frame, text='Sun Elf: +2 INT, +2 CHA',
+                                           variable=self.radio_var, value=3)
+        self.drow = tkinter.Radiobutton(self.top_frame, text='Drow: +2 DEX, +2 WIS',
+                                        variable=self.radio_var, value=4)
+        self.tiefling = tkinter.Radiobutton(self.top_frame, text='Tiefling: +2 CHA, +2 INT',
+                                            variable=self.radio_var, value=5)
+        self.dwarf = tkinter.Radiobutton(self.top_frame, text='Dwarf: +2 CON, +2 WIS',
+                                         variable=self.radio_var, value=6)
+        self.wood_elf = tkinter.Radiobutton(self.top_frame, text='Wood Elf: +2 DEX, +2 INT',
+                                            variable=self.radio_var, value=7)
+
+        self.human.pack(anchor='w', padx=20)
+        self.half_orc.pack(anchor='w', padx=20)
+        self.sun_elf.pack(anchor='w', padx=20)
+        self.drow.pack(anchor='w', padx=20)
+        self.tiefling.pack(anchor='w', padx=20)
+        self.dwarf.pack(anchor='w', padx=20)
+        self.wood_elf.pack(anchor='w', padx=20)
+
+        # Okay / Quit Buttons ----------------------------------------------------
+        self.ok_button = tkinter.Button(self.bottom_frame, text='Next', command=self.open_menu)
+        self.quit_button = tkinter.Button(self.bottom_frame, text='Quit', command=self.master.destroy)
+
+        self.ok_button.pack(side='left')
+        self.quit_button.pack(side='left')
+
+        # Pack Frames ----------------------------------------------------
+        self.upper_frame.pack()
         self.top_frame.pack()
-        self.middle_frame.pack()
         self.bottom_frame.pack()
 
-    def human_value(self):  # Don't let shadow error happen
-        try:
-            charlog_file = open(self.input_file, 'rb')
-            charlog = pickle.load(charlog_file)
-            charlog_file.close()
-        except FileNotFoundError:
-            charlog = {}
-
-        name = self.human_entry.get()
-        email = self.human_entry2.get()
-        charlog[email] = name
-
-        result = charlog.get(name, 'Customer info added.')
-        self.value.set(result)
-
-        file = open("charlog.dat", 'wb')
-        pickle.dump(charlog, file)
-        file.close()
-
-    def back(self):
-        self.human.destroy()
-
-"""
-
-
-class Human:
-    def __init__(self):
-        # self.master = master
-        self.human = tkinter.Tk()
-        # Toplevel(master)
-        self.human.title('Race, Human')
-        self.top_frame = tkinter.Frame(self.human)
-        self.left_frame = tkinter.Frame(self.human)
-        self.right_frame = tkinter.Frame(self.human)
-        self.bottom_frame = tkinter.Frame(self.human)
-
-        self.value = tkinter.StringVar()
-        self.info = tkinter.Label(self.top_frame, text='stuff here')
-        self.top_label = tkinter.Label(self.top_frame, textvariable=self.value)
-        self.info.pack(side='left')
-        self.top_label.pack(side='left')
-
-        self.value2 = tkinter.StringVar()
-        self.info2 = tkinter.Label(self.top_frame, text='picture here')
-        self.top_label2 = tkinter.Label(self.top_frame, textvariable=self.value2)
-        self.info2.pack(side='left')
-        self.top_label2.pack(side='left')
-
-        """ self.stats_section = tkinter.Label(self.left_frame, 'Stats Here')
-        self.picture_section = tkinter.Label(self.right_frame, 'Picture Here')
-        self.stats_section.pack(side='left')
-        self.right_frame.pack(side='left') """
-
-        self.roll_button = tkinter.Button(self.bottom_frame, text='Roll', command=self.back)
-        self.gen_button = tkinter.Button(self.bottom_frame, text='Generate', command=self.back)
-        self.back_button = tkinter.Button(self.bottom_frame, text='Back', command=self.back)
-
-        self.roll_button.pack(side='left')
-        self.gen_button.pack(side='left')
-        self.back_button.pack(side='left')
-
-        self.top_frame.pack()
-        self.left_frame.pack()
-        self.right_frame.pack()
-        self.bottom_frame.pack()
-        self.human.mainloop()
-
-    def back(self):
-        self.human.destroy()
+    def open_menu(self):
+        if self.radio_var.get() == 1:
+            human.Human(self.master)
+        else:
+            tkinter.messagebox.showinfo('Stats', 'still under construction')
 
 
 def main():
-    input_file = 'charlog.dat'
-    # create a window
-    # root = tkinter.Tk()
-    # call the GUI and send it the root menu
-    menu_gui = Human()
-    # control the mainloop from main instead of the class
-    # root.mainloop()
+    root = tkinter.Tk()
+    MainMenu(root)
+    root.mainloop()
 
 
 main()
